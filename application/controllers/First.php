dc24235374a92c97f71a4dd7a166ad5039ec75b5
BrynNewman <bryn.m.newman@gmail.com>
2017-02-04 13:51:41 -0800

    Fixed the routing issue with the first image

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -6,13 +6,21 @@ class First extends Application
     {
         // this is the view we want shown
         $this->data['pagebody'] = 'justone';
-
         // get the single author
         $record = $this->quotes->get(1);
-        
         // merge data with record (both associative arrays)
         $this->data = array_merge($this->data, $record);
-
         $this->render();
     }
+	
+	public function zzz(){
+		// this is the view we want shown
+		$this->data['pagebody'] = 'justone';
+		// Grab the author to pass to our view
+		$source = $this->quotes->get(1);
+        // merge data with record (both associative arrays)
+		$this->data = array_merge($this->data, $source);
+		$this->render();	
+	}
 }
+?>
